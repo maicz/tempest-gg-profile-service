d9e5f902c91ea8fb35c14b8616c9299655b53619
Mihai <zahariuc.mihai@gmail.com>
2021-05-17 17:10:59 +0300

    Initial commit

---
+++ src/main/java/com/tempest/gg/profileservice/TempestGgProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tempest.gg.profileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempestGgProfileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempestGgProfileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tempest/gg/profileservice/TempestGgProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tempest.gg.profileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempestGgProfileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
